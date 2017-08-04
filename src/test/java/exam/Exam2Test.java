9d180b0aee17eab7cfaedee20d9188d33f804f88
padungsak.k <padungsak.k@K17SRVD4E53400.kasikornbank.com>
2017-08-04 22:46:41 +0700

    get case 2A

--- src/test/java/exam/Exam2Test.java
+++ src/test/java/exam/Exam2Test.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 
 public class Exam2Test {
 	Exam2 exam2 = new Exam2();
-	
+
 	@Test
 	public void testA1() {
 		exam2.productASell();
 		int result = exam2.processPrice();
 		assertEquals(100, result);
 	}
-	
+
 	@Test
 	public void testA1B1() {
 		exam2.productASell();
@@ -21,7 +21,7 @@ public class Exam2Test {
 		int result = exam2.processPrice();
 		assertEquals(190, result);
 	}
-	
+
 	@Test
 	public void testA1B1C1() {
 		exam2.productASell();
@@ -30,7 +30,7 @@ public class Exam2Test {
 		int result = exam2.processPrice();
 		assertEquals(270, result);
 	}
-	
+
 	@Test
 	public void testA1B1C1D1() {
 		exam2.productASell();
@@ -40,7 +40,7 @@ public class Exam2Test {
 		int result = exam2.processPrice();
 		assertEquals(320, result);
 	}
-	
+
 	@Test
 	public void testA1B1C1D1E1() {
 		exam2.productASell();
@@ -51,7 +51,7 @@ public class Exam2Test {
 		int result = exam2.processPrice();
 		assertEquals(425, result);
 	}
-	
+
 	@Test
 	public void testA2() {
 		exam2.productASell();
@@ -59,13 +59,4 @@ public class Exam2Test {
 		int result = exam2.processPrice();
 		assertEquals(200, result);
 	}
-	
-	@Test
-	public void testA2() {
-		exam2.productASell();
-		exam2.productASell();
-		int result = exam2.processPrice();
-		assertEquals(200, result);
-	}
-	
 }
